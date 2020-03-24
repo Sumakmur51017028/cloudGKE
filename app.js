9b7e7ab92ee96479f7bbdb4aaac3f22e96e3572b
Sumakmur51017028 <sumakmurgoenawan_18@kharisma.ac.id>
2020-03-24 15:52:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello World?\n')
+    res.send('Sumakmur 51017028 Sistem Informasi\n')
 })
 
 app.listen(PORT, HOST)

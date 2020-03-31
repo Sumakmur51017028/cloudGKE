83cdc8bf10a3c61c595abc60e23f4db8124d365c
Sumakmur51017028 <sumakmurgoenawan_18@kharisma.ac.id>
2020-03-31 12:36:05 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 'use strict'
 
 const express = require('express')
-const fs = require('fs')
+
 
 const PORT = 8000
 const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.writeHead(200,{'Content-Type': 'text/html'})
-    fs.readFile('index.html')
-    res.write('Sumakmur 51017028 Sistem Informasi12345')
+
+
+    res.write('Sumakmur 51017028 Sistem Informasi')
     res.end()
 })
 
